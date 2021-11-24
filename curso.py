083ce3a0eb93ced5e7b4c94e7eca3cbfdfaa88f6
nikolaskaracsonyi <nikolas.s.karacsonyi@gmail.com>
2021-11-24 17:10:52 -0300

    wrfwfrwe

---
+++ curso.py
@@ -0,0 +1,4 @@
+nome = input("Hello, what's your name?")
+print(f'Welcome {nome}, good to see you here')
+idade = input("How old are you ?")
+print(f"Nice,{idade} years are the best time of the life!")
