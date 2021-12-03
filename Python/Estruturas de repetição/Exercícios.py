7a73a6b03507b017e8fa2ac2d83a82f65b825a72
nikolaskaracsonyi <nikolas.s.karacsonyi@gmail.com>
2021-12-03 00:34:43 -0300

    Adding exercises

---
+++ Python/Estruturas de repetição/Exercícios.py
@@ -0,0 +1,36 @@
+"""
+# 2.
+
+for num in range(1, 101):
+    print(num)
+
+
+numero = 0
+
+while numero < 101:
+    print(numero)
+    numero = numero + 1
+
+# 3.
+
+numero = 10
+
+while numero != -1:
+    print(numero)
+    numero = numero - 1
+    if numero == -1:
+        print('FIM!')
+
+# 4.
+
+numero = 0
+
+while numero < 101000:
+    print(numero)
+    numero = numero + 1000
+
+"""
+
+
+
+
