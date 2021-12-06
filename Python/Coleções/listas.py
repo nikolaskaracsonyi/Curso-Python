63dccabababacf3fc7444f5086cf6683c1565a2b
nikolaskaracsonyi <nikolas.s.karacsonyi@gmail.com>
2021-12-06 03:02:13 -0300

    Listas: parte 1

---
+++ Python/Coleções/listas.py
@@ -0,0 +1,17 @@
+"""
+Listas
+
+Listas em Python funcionam como vetores/matrizes (arrays) em outras linguagens, com a diferença
+de serem DINÂMICOS e também de podermos colocar QUALQUER tipo de dado.
+
+Linguagens C/Java: Arrays
+     - Possuem tamanho e tipo de dado fixo
+     Ou seja, nestas linguaguens se você criar um array do tipo int e com tamanho 5, este array
+     será SEMPRE do tipo inteiro e poderá ter SEMPRE no máximo 5 valores.
+
+Python
+- Dinâmico: Não possuem tamanho fixo; Ou seja, podemos criar a lista e simplesmente ir adicionando elementos;
+- Qualquer tipo de dado: Não possuem tipo de dado fixo, ou seja, podemos colocar qualquer tipo de dado;
+
+As listas em Python são representadas por colchetes : []
+"""
