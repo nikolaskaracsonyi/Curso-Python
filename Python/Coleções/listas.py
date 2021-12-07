30dcba9fd0e967ddc2058a00f42c62661fbad8f7
nikolaskaracsonyi <nikolas.s.karacsonyi@gmail.com>
2021-12-06 21:16:26 -0300

    adding content

--- Python/Coleções/listas.py
+++ Python/Coleções/listas.py
@@ -15,3 +15,22 @@ Python
 
 As listas em Python são representadas por colchetes : []
 """
+
+print(type([]))
+
+lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
+
+lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n',
+          'i', 'v', 'e', 'r', 's', 'i', 't', 'y']
+
+lista3 = []
+
+lista4 = list(range(11))
+
+lista5 = list('Geek University')
+
+# Podemos facilmente checar se determinado valor está contido na lista
+if 8 in lista4:
+    print('Encontrei o número 8')
+else:
+    print('Não encontrei o número 8')
