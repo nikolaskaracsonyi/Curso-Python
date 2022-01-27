77924372d5d4675597971e26092def6c0f1abf59
nikolaskaracsonyi <nikolas.s.karacsonyi@gmail.com>
2022-01-27 00:54:40 -0300

    adding content to collections (dict, maps)

--- Python/Coleções/dicionários.py
+++ Python/Coleções/dicionários.py
@@ -164,8 +164,6 @@ Carrinho de Compras:
         - quantidade;
         - preço;
 
-"""
-
 # Forma 1 - Poderiamos utilizar uma Lista
 
 carrinho = []
@@ -202,3 +200,63 @@ carrinho.append(produto1)
 carrinho.append(produto2)
 
 print(carrinho)
+
+# Dessa forma facilmente adicionamos ou removemos produtos no carrinho e em cada produto podemos ter
+# a certeza sobre cada informação.
+
+# Métodos de dicionários.
+
+d = dict(a=1, b=2, c=3)
+
+print(d)
+print(type(d))
+
+# Limpar o dicionário (Zerar dados)
+
+d.clear()
+print(d)
+
+# Copiando um dicionário para outro
+
+# Forma 1 - Deep Copy
+
+novo = d.copy() 
+
+print(novo)
+
+novo['d'] = 4
+
+print(d)
+print(novo)
+
+# Forma 2 -  Shallow Copy
+
+novo = d
+print(novo)
+
+novo['d'] = 4
+
+print(d)
+print(novo)
+"""
+
+# Forma não usual de criação de dicionários
+
+outro = {}.fromkeys('a', 'b')
+
+print(outro)
+print(type(outro))
+
+
+usuario = {}.fromkeys(['nome', 'pontos', 'email', 'profile'], 'desconhecido')
+print(usuario)
+print(type(usuario))
+
+# O método .fromkeys recebe dois parâmetros: um iterável e um valor.
+# Ele vai gerar para cada valor do iterável uma chave e irá atribuir a esta chave o valor informado.
+
+veja = {}.fromkeys('teste', 'valor')
+print(veja)
+
+veja = {}.fromkeys(range(1, 11),'novo')
+print(veja)
