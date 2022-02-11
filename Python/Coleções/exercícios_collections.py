38a1889c2ab20c66e357c5b561d6c5a658769f22
nikolaskaracsonyi <nikolas.s.karacsonyi@gmail.com>
2022-02-10 22:43:03 -0300

    exercises

---
+++ Python/Coleções/exercícios_collections.py
@@ -0,0 +1,23 @@
+"""
+Exercícios
+
+EX 1 -
+
+a = [1, 0, 5, -2, -5, 7]
+
+soma = a[0] + a[1] + a[5]
+
+print(soma)
+
+a[4] = 100
+
+for values in a:
+    print(values)
+
+EX 2 -
+valores = [10, 23, 64, 75, 12, 4]
+print(valores)
+
+for int in valores:
+    print(int)
+"""
